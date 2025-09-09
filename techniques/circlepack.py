36bf7b735ce9d2509fa6e50536182457000cb69e
Abbey Diller <dillerab@mail.gvsu.edu>
2025-09-09 11:52:04 -0400

    add circle packing technique

---
+++ techniques/circlepack.py
@@ -0,0 +1,97 @@
+import drawsvg
+import math
+import shapely
+
+from base_technique import BaseTechnique
+
+class CirclePacking(BaseTechnique):
+    def __init__(self, rng, subdim, palette, n_spawn, max_failures, start_r, pad=1):
+        super().__init__(rng, subdim, palette)
+        self.n_spawn = n_spawn
+        self.max_failures = max_failures
+        self.start_r = start_r  # starting radius of new circles
+        self.pad = pad  # minimum spacing between circles
+
+        self.circles = []
+    
+
+    def spawn_circle(self):
+        """
+        Attempt to place new circle in a random, unoccupied location.
+        """
+        buffer = self.start_r + self.pad   # buffer distance from edge of canvas
+        failures = 0
+        spawned = False
+        while (not spawned) and failures < self.max_failures:
+            x = self.rng.randrange(buffer, self.width - buffer)
+            y = self.rng.randrange(buffer, self.height - buffer)
+            x += self.origin['x']
+            y += self.origin['y']
+            #print(str(x) + ", " + str(y))
+
+            if self.collision({'x': x, 'y': y, 'r': self.start_r}):
+                failures += 1
+            else:
+                # create new circle
+                c = {
+                    'x': x,
+                    'y': y,
+                    'r': self.start_r,
+                    'growing': True,
+                    'colour': self.rng.choice(self.palette)
+                }
+                self.circles.append(c)
+                spawned = True
+        if failures >= self.max_failures:
+            return False
+        else:
+            return True
+    
+
+    def collision(self, c):
+        """
+        Check if a given circle collides with any other circle or canvas edge.
+        """
+        circle_collision = False
+        
+        for c2 in self.circles:
+            if c != c2:    
+                distance = math.dist((c['x'], c['y']), (c2['x'], c2['y']))
+                if distance < c['r'] + c2['r'] + (self.pad * 2):
+                    circle_collision = True
+        
+        edge_collision = ((c['x'] - c['r'] - self.pad <= self.origin['x']) or
+                          (c['x'] + c['r'] + self.pad >= self.origin['x'] + self.width) or
+                          (c['y'] - c['r'] - self.pad <= self.origin['y']) or
+                          (c['y'] + c['r'] + self.pad >= self.origin['y'] + self.height))
+        return circle_collision or edge_collision
+    
+
+    def draw(self, d):
+        terminated = False
+        while not terminated:
+            for _ in range(self.n_spawn):
+                new_placed = self.spawn_circle()
+                terminated = not new_placed
+                if terminated:
+                    break
+            if not terminated:
+                # grow circles that have not yet collided with other circles or canvas edge
+                for c in self.circles:
+                    if c['growing']:
+                        if self.collision(c):
+                            c['growing'] = False
+                        else:
+                            c['r'] += 1
+        else:
+            for c in self.circles:
+                cx = c['x']
+                cy = c['y']
+                r = c['r']
+                colour = c['colour']
+                # add to SVG
+                d.append(drawsvg.Circle(cx, cy, r,
+                                        fill='none', stroke=colour, stroke_weight=1))
+                # approximate circles as point buffers 
+                self.geoms.append(shapely.Point(cx, cy).buffer(r).boundary)
+        return d
--- techniques/technique_tester.py
+++ techniques/technique_tester.py
@@ -5,6 +5,7 @@ import drawsvg
 import random
 
 from eca import ElementaryCA
+from circlepack import CirclePacking
 
 test_palette = ["#61E8E1", "#F25757", "#FFC145", "#1F5673"]
 
@@ -16,11 +17,15 @@ def main():
     sd1 = (0, 0, 300, 300)
     sd2 = (300, 300, 300, 300)
 
-    e = ElementaryCA(rng, sd1, test_palette, 20)
-    e.draw(d)
-    e2 = ElementaryCA(rng, sd2, test_palette, 40)
-    e2.draw(d)
-    d.save_svg("test-eca.svg")
+    #e = ElementaryCA(rng, sd2, test_palette, 20)
+    #e.draw(d)
+    #d.save_svg("test-eca.svg")
+    
+    cp = CirclePacking(rng, sd1, test_palette, 1, 200, 2)
+    cp.draw(d)
+    cp2 = CirclePacking(rng, sd2, test_palette, 1, 200, 2)
+    cp2.draw(d)
+    d.save_svg("test-circlepack.svg")
 
 
 if __name__ == "__main__":
