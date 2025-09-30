a76749f2b2d575356bd9bcb6e8c3204b427a11d3
Abbey Diller <dillerab@mail.gvsu.edu>
2025-09-29 22:16:25 -0400

    adjust how canvas sub-area width and height are calculated

--- techniques/base_technique.py
+++ techniques/base_technique.py
@@ -4,8 +4,8 @@ class BaseTechnique(ABC):
     def __init__(self, rng, subdim, palette):
         self.rng = rng
         self.origin = {'x': subdim[0], 'y': subdim[1]}
-        self.width = subdim[2]
-        self.height = subdim[3]
+        self.width = subdim[2] - subdim[0]
+        self.height = subdim[3] - subdim[1]
         self.palette = palette
 
         self.geoms = []
