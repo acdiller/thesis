d4fd4a1990b46bb62af591624a97c0c38bc73ecb
Abbey Diller <dillerab@mail.gvsu.edu>
2025-10-26 19:10:52 -0400

    add penwidth attribute

--- individual.py
+++ individual.py
@@ -1,9 +1,11 @@
 class Individual:
-    def __init__(self, id, rng, dim, drawing, techniques):
+    def __init__(self, id, rng, dim, penwidth, drawing, techniques):
         self.id = id
         self.rng = rng
         self.dim = dim
         self.drawing = drawing
+        self.penwidth = penwidth
+        
         self.techniques = techniques
 
         self.features = None
