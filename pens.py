692a7a2aa7e3dbbed741f144dc0669f960868de0
Abbey Diller <dillerab@mail.gvsu.edu>
2025-10-26 21:29:19 -0400

    add some pens + colours

---
+++ pens.py
@@ -0,0 +1,75 @@
+# dictionaries containing information about different available pens for plotting
+# pen width is in mm
+
+pilotV5 = {
+    'penwidth': 0.25,
+    'colours': {
+        'pink': '#FC0FC0',
+        'red': '#FF0000',
+        'orange': '#FF7F00',
+        'yellow': '#FFFF00',
+        'limegreen': '#32CD32',
+        'green': '#008000',
+        'turquoise': '#0FC0FC',
+        'blue': '#002395',
+        'purple': '#8A2BE2',
+    }
+}
+
+pilotV7 = {
+    'penwidth': 0.35,
+    'colours': {
+        'salmon': '#FC5A8D',
+        'cherry': '#B22222',
+        'red': '#FF0000',
+        'honey': '#FFC94F',
+        'emerald': '#50C878',
+        'lightblue': '#77B5FE',
+        'blue': '#002395',
+        'lilac': '#C16AC1'
+    }
+}
+
+gellyroll06 = {
+    'penwidth': 0.4,    # manufacturer - 0.35
+    'colours': {
+        'fluo-pink': '#FF70BD',
+        'fluo-vermilion': '#FF7676',
+        'rose': '#C71585',
+        'red': '#CC3333',
+        'fluo-orange': '#FF9D44',
+        'fluo-yellow': '#EEFF1B',
+        'freshgreen': '#9DC209',
+        'fluo-green': '#00FF00',
+        'leafgreen': '#589828',
+        'green': '#00BDA5',
+        'bluegreen': '#007667',
+        'skyblue': '#87CEEB',
+        'blue': '#1F75FE',
+        'ultramarine': '#1034A6',
+        'lavender': '#DBC6EA',
+        'purple': '#8A2BE2',
+        'bordeaux': '#66023C'
+    }
+}
+
+gellyroll10 = {
+    'penwidth': 0.6,    # manufacturer - 0.5
+    'colours': {
+        'fluo-pink': '#FF70BD',
+        'fluo-vermilion': '#FF7676',
+        'rose': '#C71585',
+        'red': '#CC3333',
+        'fluo-orange': '#FF9D44',
+        'fluo-yellow': '#EEFF1B',
+        'fluo-green': '#00FF00',
+        'green': '#00BDA5',
+        'blue': '#1F75FE',
+        'purple': '#8A2BE2',
+        'p-pink': '#FFCFF1',
+        'p-orange': '#FFDE7C',
+        'p-yellow': '#FDFF70',
+        'p-green': '#88FFC5',
+        'p-periwinkle': '#A8AAFF'
+    }
+}
